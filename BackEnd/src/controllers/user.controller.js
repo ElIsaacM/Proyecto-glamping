0b6cebd7354f90f27c399bb1a9c8a667ae3c1645
Carlos Tabares <cmtm954@gmail.com>
2026-03-08 17:00:13 -0500

    modulo usuarios

--- BackEnd/src/controllers/user.controller.js
+++ BackEnd/src/controllers/user.controller.js
@@ -19,3 +19,56 @@ export const getuserById = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const createUser = async (req, res) => {
+  try {
+    const data = req.body;
+    const newUser = await userService.createUser(data)
+
+    res.status(201).json({
+      message: 'Usuario creado exitosamente.',
+      data: newUser
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al crear usuario',
+      error: error.message
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updatedUser = await userService.updateUser(id, data);
+
+    res.status(201).json({
+      message: 'Usuario actualizado',
+      userID: id,
+      data: updatedUser
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al actualizar usuario',
+      error: error.message
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await userService.deleteUser(id)
+
+    res.status(200).json({
+      message: 'Usuario eliminado',
+      userID: id
+  });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al eliminar usuario',
+      error: error.message
+    });
+  }
+};
